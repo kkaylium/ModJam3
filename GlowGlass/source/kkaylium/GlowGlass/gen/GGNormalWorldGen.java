f65be412108b4a4e2d6cf2658421188b85f32f12
kkaylium <kkaylium13@hotmail.com>
2013-12-13 13:26:47 -0700

    Next Commit

--- GlowGlass/source/kkaylium/GlowGlass/gen/GGNormalWorldGen.java
+++ GlowGlass/source/kkaylium/GlowGlass/gen/GGNormalWorldGen.java
@@ -2,49 +2,50 @@ package kkaylium.GlowGlass.gen;
 
 import java.util.Random;
 
+import kkaylium.GlowGlass.blocks.GGBlocks;
+
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.gen.feature.WorldGenMinable;
 import cpw.mods.fml.common.IWorldGenerator;
 
 public class GGNormalWorldGen implements IWorldGenerator{
 
 	@Override
-	public void generate(Random random, int chunkX, int chunkZ, World world,IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
+	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
 		
 		switch(world.provider.dimensionId)
 		{	
-			case: -1
-			generateNether();
+			case -1:
+			generateNether(world, random, chunkX * 16, chunkZ * 16);
 			break;
-			case: 0
-			generateOverworld(world random, chunkX * 16, chunkZ * 16);
+			case 0:
+			generateOverworld(world, random, chunkX * 16, chunkZ * 16);
 			break;
-			case: 1
+			case 1:
 			generateEnd();
 			break;
 		}
+	}
+
+	public void generateOverworld(World world, Random random, int chunkX, int chunkZ) {
 		
-		public void generateNether()
+		for(int i = 0; i < 20; i++)
 		{
+			int oreX = chunkX + random.nextInt(16); 
+			int oreY = random.nextInt(40);
+			int oreZ = chunkZ + random.nextInt(16);
 			
-		}
-		
-		public void generateOverworld(World world, Random random, int chunkX, int chunkZ)
-		{
-			for(int i = 0; i < 20; i++)
-			{
-				int oreX = chunkX + random.nextInt(16);
-				int oreY = random.nextInt(40);
-				int oreZ = chunkZ + random.nextInt(16);
-				
-				
-			}
-		}
-		
-		public void generateEnd()
-		{
-			
+			(new WorldGenMinable(GGBlocks.GlowOre.blockID, 10)).generate(world, random, oreX, oreY, oreZ);
 		}
 	}
 
+	public void generateNether(World world, Random random, int i, int j) {
+		
+	}
+	
+	private int generateEnd() {
+		
+		return 0;
+	}
 }
