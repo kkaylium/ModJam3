b3dd3a31d4a62d5a077d008b8ef0384fdd392ed8
kkaylium <kkaylium13@hotmail.com>
2013-12-13 07:31:47 -0700

    Again, Late, sorry :(

--- GlowGlass/source/kkaylium/GlowGlass/items/ItemGlowCrystal.java
+++ GlowGlass/source/kkaylium/GlowGlass/items/ItemGlowCrystal.java
@@ -1,17 +1,62 @@
 package kkaylium.GlowGlass.items;
 
+import java.util.List;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Icon;
+import net.minecraft.util.MathHelper;
 
 public class ItemGlowCrystal extends Item{
 
+	private String[] crystalNames = new String[] {"WHITE", "BLACK"};
+	
+	@SideOnly(Side.CLIENT)
+	private Icon[] icons;
+	
 	public ItemGlowCrystal(int par1) {
 		super(par1);
 		this.setHasSubtypes(true);
+		this.setMaxDamage(0);
 		this.setCreativeTab(CreativeTabs.tabMaterials);
 		
 	}
 	
-
+	@Override
+	public String getUnlocalizedName(ItemStack itemStack)
+	{
+		StringBuilder unlocalizedName = new StringBuilder();
+		int meta = MathHelper.clamp_int(itemStack.getItemDamage(), 0, 1);
+		
+		unlocalizedName.append("item.");
+		unlocalizedName.append("glowglass:");
+		unlocalizedName.append("GlowCrystal");
+		unlocalizedName.append(crystalNames[meta]);
+		
+		return unlocalizedName.toString();
+		
+	}
+	
+	@SideOnly(Side.CLIENT)
+	public void getSubItems(int par1, CreativeTabs par2CreativeTabs, List par3List)
+	{
+		
+	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IconRegister iconRegister)
+	{
+		this.icons = new Icon[crystalNames.length];
+		
+		for(int i = 0; i < crystalNames.length; i++)
+		{
+			icons[i] = iconRegister.registerIcon("glowglass:GlowCrystal"+ crystalNames[i]);
+		}
+	}
 	
 }
