98ac22b58e28b86d83b982c2e1bd185e95ce70f5
kkaylium <kkaylium13@hotmail.com>
2013-12-13 21:25:12 -0700

    Commit Again

--- GlowGlass/source/kkaylium/GlowGlass/blocks/BlockGlowFence.java
+++ GlowGlass/source/kkaylium/GlowGlass/blocks/BlockGlowFence.java
@@ -1,6 +1,7 @@
 package kkaylium.GlowGlass.blocks;
 
 import java.util.List;
+import java.util.Random;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -30,6 +31,7 @@ public class BlockGlowFence extends BlockFence{
 		this.field_94464_a = par2String;
 		this.setCreativeTab(GlowGlass.GGTab);
 		this.setUnlocalizedName("GlowFence");
+		this.setTickRandomly(true);
 		this.setLightValue(1.0F);
 	}
 	
@@ -64,6 +66,59 @@ public class BlockGlowFence extends BlockFence{
 		for(int i = 0; i < this.icons.length; i++)
 		{
 			this.icons[i] = iconRegister.registerIcon("glowglass:" + this.field_94464_a + fenceNames[i]);
+		}
+	}
+	
+	public void randomDisplayTick(World world, int par2, int par3, int par4, Random random)
+	{
+		this.sparkle(world, par2, par3, par4);
+	}
+	
+	private void sparkle(World world, int par2, int par3, int par4)
+	{
+		Random random = world.rand;
+		double d0 = 0.0625D;
+		for(int i = 0; i < 1; i++)
+		{
+			double d1 = (double)((float)par2 + random.nextFloat());
+			double d2 = (double)((float)par3 + random.nextFloat());
+			double d3 = (double)((float)par4 + random.nextFloat());
+			
+			if(i == 0 && !world.isBlockOpaqueCube(par2, par3 + 1, par4))
+			{
+				d2 = (double)(par3 + 1) + d0;
+			}
+			
+			if(i == 1 && !world.isBlockOpaqueCube(par2, par3 - 1, par4))
+			{
+				d2 = (double)(par3 + 0) - d0;
+			}
+			
+			if(i == 2 && !world.isBlockOpaqueCube(par2, par3, par4 + 1))
+			{
+				d3 = (double)(par4 + 1) + d0;
+			}
+			
+			if(i == 3 && !world.isBlockOpaqueCube(par2, par3, par4 - 1))
+			{
+				d3 = (double)(par4 + 0) - d0;
+			}
+			
+			if(i == 4 && !world.isBlockOpaqueCube(par2 + 1, par3, par4))
+			{
+				d1 = (double)(par2 + 1) + d0;
+			}
+			
+			if(i == 5 && !world.isBlockOpaqueCube(par2 - 1, par3, par4))
+			{
+				d1 = (double)(par2 + 0) - d0;
+			}
+			
+			if (d1 < (double)par2 || d1 > (double)(par2 + 1) || d2 < 0.0D || d2 > (double)(par3 + 1) || d3 < (double)par4 || d3 > (double)(par4 + 1))
+			{
+				world.spawnParticle("reddust", d1, d2, d3, 0.7D, 0.9D, 1.0D);
+			}
+			
 		}
 	}
 	
