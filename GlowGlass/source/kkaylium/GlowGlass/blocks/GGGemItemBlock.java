75f17b4b34887a79d113173c5a7e281306600720
kkaylium <kkaylium13@hotmail.com>
2013-12-16 19:28:11 -0700

    Final Commit?

---
+++ GlowGlass/source/kkaylium/GlowGlass/blocks/GGGemItemBlock.java
@@ -0,0 +1,33 @@
+package kkaylium.GlowGlass.blocks;
+
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.MathHelper;
+
+public class GGGemItemBlock extends ItemBlock{
+	
+
+	public GGGemItemBlock(int par1) {
+		super(par1);
+		
+	}
+	@Override
+	public int getMetadata(int par1)
+	{
+		return par1;
+	}
+	@Override
+	public String getUnlocalizedName(ItemStack itemStack)
+	{
+		StringBuilder unlocalizedName = new StringBuilder();
+		int meta = MathHelper.clamp_int(itemStack.getItemDamage(), 0, 10);
+		
+		unlocalizedName.append("item.");
+		unlocalizedName.append("glowglass:");
+		unlocalizedName.append("GlowBlock");
+		unlocalizedName.append(GGGemBlock.blockNames[meta]);
+		
+		return unlocalizedName.toString();
+	}
+
+}
